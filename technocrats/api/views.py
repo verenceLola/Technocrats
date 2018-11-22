d7a73f87bf778721a3ded298d57026ba15b589f0
vincent Onyango <vincent.onyango@andela.com>
2018-11-22 14:49:20 +0300

    feat(auth): add user authentication

--- technocrats/api/views.py
+++ technocrats/api/views.py
@@ -3,19 +3,27 @@
 from rest_framework import generics
 from .serializers import BucketlistSerializer
 from .models import Bucketlist
+from .permissions import IsOwner
+from rest_framework import permissions
 
 
 class CreateView(generics.ListCreateAPIView):
     """This class defines the create behavior of our rest api."""
     queryset = Bucketlist.objects.all()
     serializer_class = BucketlistSerializer
+    permission_classes = (
+        permissions.IsAuthenticated, IsOwner)
+
 
     def perform_create(self, serializer):
         """Save the post data when creating a new bucketlist."""
-        serializer.save()
+        serializer.save(owner_id=self.request.user)
 
 class DetailsView(generics.RetrieveUpdateDestroyAPIView):
     """This class handles the http GET, PUT and DELETE requests."""
 
     queryset = Bucketlist.objects.all()
     serializer_class = BucketlistSerializer
+    permission_classes = (
+        permissions.IsAuthenticated,
+        IsOwner)
